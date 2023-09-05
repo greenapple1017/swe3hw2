325afc298f1b81885837dc87c0ced4ae4d95a98a
greenapple1017 <1017greenapple@gmail.com>
2023-09-05 20:10:18 +0900

    change count.cc

--- count.cc
+++ count.cc
@@ -1,6 +1,8 @@
 #include "count.h"
 
 void count_number() {
-    // lab2 //
-    // do it yourself //
+    for (int i = 0; i < 10; i++) 
+        cout << i+1 << endl;
+    cout << 2015312447 << endl;
+    return 0;
 }
